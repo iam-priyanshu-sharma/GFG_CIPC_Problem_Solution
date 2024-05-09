da4816527846d600324e10e3670bd48bd9ae8414
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2024-05-09 15:43:56 +0530

    Hello World

---
+++ Java/Basics/Hello World/src/Main.java
@@ -0,0 +1,15 @@
+class Geeks {
+    // Function to print hello
+    static void printHello () {
+
+        // Your code here
+        System.out.print ("Hello World");
+    }
+}
+
+class Main {
+    public static void main (String[] args) {
+        //Calling printHello() function of the Class Geeks
+        Geeks.printHello ();
+    }
+}
