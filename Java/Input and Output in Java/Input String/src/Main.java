8bbe6f61db2c89050e0f12fc4ae4214245a68181
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2024-05-11 08:16:06 +0530

    Input String

---
+++ Java/Input and Output in Java/Input String/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main (String[] args) throws IOException {
+        BufferedReader read = new BufferedReader (new InputStreamReader (System.in));
+
+        System.out.print ("Enter number of test cases you want to perform: ");
+        int t = Integer.parseInt (read.readLine ());
+
+        while (t-- > 0) {
+            System.out.print ("Enter a String: ");
+            String myInput = read.readLine ();
+            System.out.println (myInput);
+        }
+    }
+}
