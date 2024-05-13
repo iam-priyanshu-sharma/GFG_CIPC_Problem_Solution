bd2fcdaf4336678c76fc40878198981578e841d1
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2024-05-13 15:53:01 +0530

    The IF statement

---
+++ Java/Flow Control/The IF Statement/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void utility (int number) {
+        //Write your if statement below
+        if ( number > 100 ) {
+            System.out.println ("Big");
+        }
+
+        //Write your if statement above
+        System.out.println ("Number");
+    }
+
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.print ("Enter number of test cases you want to perform: ");
+        int t = scanner.nextInt ();
+
+        while (t-- > 0) {
+            System.out.print ("Enter a number: ");
+            int number = scanner.nextInt ();
+            utility (number);
+        }
+        scanner.close ();
+    }
+}
