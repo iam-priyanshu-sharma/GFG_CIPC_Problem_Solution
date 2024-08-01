e5c75906a44004b9a207a65d6b4d0a168c2bf2ad
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2024-08-01 18:04:48 +0530

    Less Than

--- Java/Arrays/Less Than/src/Main.java
+++ Java/Arrays/Less Than/src/Main.java
@@ -2,19 +2,29 @@ import java.util.Scanner;
 
 class Main {
 
-    public static int[] decrementArrayElements (int[] numbers) {
-        int[] dec = new int[numbers.length];
-
-        for (int i = 0; i < numbers.length; i++) {
-            dec[i] = numbers[i] - 1;
+    public static int[] lessThan (int[] numbers, int k) {
+        int count = 0;
+        for (int value : numbers) {
+            if ( value < k ) {
+                count++;
+            }
         }
-        return dec;
+
+        int[] arr = new int[count];
+        int index = 0;
+        for (int value : numbers) {
+            if ( value < k ) {
+                arr[index++] = value;
+            }
+        }
+        return arr;
     }
 
     public static void main (String[] args) {
         Scanner scanner = new Scanner (System.in);
         System.out.print ("Enter number of test cases you want to perform: ");
         int t = scanner.nextInt ();
+
         while (t-- > 0) {
             System.out.print ("Enter length of array: ");
             int n = scanner.nextInt ();
@@ -25,10 +35,12 @@ class Main {
                 numbers[i] = scanner.nextInt ();
             }
 
-            int[] res = decrementArrayElements (numbers);
+            System.out.print ("Enter a number: ");
+            int k = scanner.nextInt ();
+            int[] res = lessThan (numbers, k);
 
-            for (int i = 0; i < n; i++) {
-                System.out.print (res[i] + " ");
+            for (int re : res) {
+                System.out.print (re + " ");
             }
             System.out.println ();
         }
