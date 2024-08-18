4a3887ddc173f669bfad85d6e639d94f21736d7b
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2024-08-18 17:36:00 +0530

    Constructor

---
+++ Java/OOPs/Constructor/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class User {
+    String name;
+
+    User () {
+        this.name = "Default";
+    }
+}
+
+class Main {
+    public static void main (String[] args) {
+        Scanner ob = new Scanner (System.in);
+        System.out.print ("Enter number of test cases you want to perform: ");
+        int t = ob.nextInt ();
+
+        while (t-- > 0) {
+            User user = new User ();
+            System.out.println (user.name);
+        }
+    }
+}
