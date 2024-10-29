ce9dad4ad986eff0d3e7636088212c270746eb3b
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2024-10-29 15:42:03 +0530

    Right Angle Triangle 2

---
+++ Java/Loops/Right Angle Triangle 2/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void triangle (int s) {
+        for (int i = 1; i <= s; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ( i == 1 || i == s || j == 1 || j == i ) {
+                    System.out.print ("* ");
+                } else {
+                    System.out.print ("  ");
+                }
+            }
+            System.out.println ();
+        }
+    }
+
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.print ("Enter number of test cases you want to perform: ");
+        int t = scanner.nextInt ();
+
+        while (t-- > 0) {
+            System.out.print ("Enter value of s: ");
+            int s = scanner.nextInt ();
+            triangle (s);
+            System.out.println ("~");
+        }
+        scanner.close ();
+    }
+}
