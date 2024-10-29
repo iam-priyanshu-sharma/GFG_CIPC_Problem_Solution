20a4f1a310f7cca3ab6ea1c63c2221bf48e247e7
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2024-10-29 15:48:39 +0530

    Inverted Right Angle Triangle

---
+++ Java/Loops/Inverted Right Angle Triangle/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void invTriangleWall (int s) {
+        for (int i = s; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print ("* ");
+            }
+            System.out.println ();
+        }
+    }
+
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.print ("Enter number of test cases you want to perform: ");
+        int t = scanner.nextInt ();
+
+        while (t-- > 0) {
+            System.out.print ("Enter value of s: ");
+            int s = scanner.nextInt ();
+            invTriangleWall (s);
+            System.out.println ("~");
+        }
+        scanner.close ();
+    }
+}
